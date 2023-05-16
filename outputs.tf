4cdd93d3e9f870a25b534d90d5c25d2d2e6076dc
Jonatan Sombié <jonatan.sombie@gmail.com>
2023-05-16 13:13:13 +0200

    use generic names for exported env

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
-output "COGNITO_CLIENT_ID" {
+output "OIDC_CLIENT_ID" {
   value     = aws_cognito_user_pool_client.client.id
 }
 
-output "COGNITO_CLIENT_SECRET" {
+output "OIDC_CLIENT_SECRET" {
   value = {
     type   = "ssm"
     key    = local.client_secret_parameter_key
@@ -10,22 +10,22 @@ output "COGNITO_CLIENT_SECRET" {
   }
 }
 
-output "COGNITO_ISSUER_URL" {
+output "OIDC_ISSUER_URL" {
   value     = "https://cognito-idp.${data.aws_region.current.name}.amazonaws.com/${var.cognito_user_pool_id}"
 }
 
-output "COGNITO_DISCOVERY_URL" {
+output "OIDC_DISCOVERY_URL" {
   value     = "https://cognito-idp.${data.aws_region.current.name}.amazonaws.com/${var.cognito_user_pool_id}/.well-known/openid-configuration"
 }
 
-output "COGNITO_LOGOUT_URL" {
+output "OIDC_LOGOUT_URL" {
   value     = "${var.cognito_user_pool_domain}/logout"
 }
 
-output "COGNITO_LOGOUT_REDIRECT_URL" {
+output "OIDC_LOGOUT_REDIRECT_URL" {
   value = tolist(aws_cognito_user_pool_client.client.logout_urls)[0]
 }
 
-output "COGNITO_CALLBACK_URL" {
+output "OIDC_CALLBACK_URL" {
   value = tolist(aws_cognito_user_pool_client.client.callback_urls)[0]
 }
